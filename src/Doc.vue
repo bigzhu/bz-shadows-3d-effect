9fb0a81068a3b7adbd03ca3bbfdc390300d5b2a4
周汉成 <308114274@qq.com>
2016-12-24 14:12:42 +0800

    更新文档

--- src/Doc.vue
+++ src/Doc.vue
@@ -6,45 +6,28 @@
       :parms="parms"
       :code="code"
       >
-      <bz :loading="loading" v-on:check_done="call_back"></bz>
+      <shadows-3d-effect></shadows-3d-effect>
     </doc>
   </div>
 </template>
 <script>
-  import toastr from 'toastr'
-
-  import Bz from './Bz'
+  import Shadows3dEffect from './Bz'
   import Doc from 'bz-doc'
   export default {
     components: {
-      Bz,
+      Shadows3dEffect,
       Doc
     },
     data: function () {
       return {
-        loading: false,
-        name: 'bz-login',
-        desc: '登录页面',
-        parms: [
-          {parm: 'check_done', desc: 'v-on 绑定 check_done 事件，完成了用户名密码必填的check'},
-          {parm: 'user_name_placeholder', desc: '可选'},
-          {parm: 'password_placeholder', desc: '可选'},
-          {parm: 'loading', desc: '控制按钮loading效果(可选)'}
-        ],
+        name: 'bz-shadows-3d-effect',
+        desc: '3d效果图片',
+        parms: [],
         parm_desc: ``,
-        code: `<bz v-on:check_done="call_back"></bz>`
+        code: `<shadows-3d-effect></shadows-3d-effect>`
       }
     },
     methods: {
-      call_back: function (user_name, password) {
-        this.loading = true
-        let _this = this
-        window.setTimeout(function () {
-          window.alert(`user_name:${user_name}, password: ${password}`)
-          toastr.info('登录成功')
-          _this.loading = false
-        }, 2000)
-      }
     }
   }
 </script>
